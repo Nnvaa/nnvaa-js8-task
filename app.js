77e6e6f82fb327e5168432be1bbc28562e6ba3c1
Nnvaa <nigarnva2005@gmail.com>
2025-02-28 14:53:19 +0400

    task 8 njs

---
+++ app.js
@@ -0,0 +1,28 @@
+let n = 100
+while(n <= 990){
+    if(n % 10 == 0){
+
+console.log(n)
+n = n + 10
+    }
+}
+
+
+
+
+
+
+
+let a = 11;
+let num = 0;
+
+
+while (a <= 99) {
+  a = a + 2; 
+  num += a; 
+}
+
+console.log(num); 
+
+
+
