49c58c1b53d7c773e2e5ac187a51d3ccb2ec4a20
Nnvaa <nigarnva2005@gmail.com>
2025-02-28 19:13:27 +0400

    nnvaa js8 taskk

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-let n = 100
-while(n <= 990){
-    if(n % 10 == 0){
+// let n = 100
+// while(n <= 990){
+//     if(n % 10 == 0){
 
-console.log(n)
-n = n + 10
-    }
-}
-
-
+// console.log(n)
+// n = n + 10
+//     }
+// }
 
 
 
 
 
-let a = 11;
+
+
+let a = 9;
 let num = 0;
 
 
-while (a <= 99) {
+while (a <= 97) {
   a = a + 2; 
   num += a; 
 }
